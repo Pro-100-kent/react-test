e6f3f89c7824b20a16603fae04ad129bc2088b2b
Pro-100-kent <pro100kent@gmail.com>
2019-12-04 17:09:03 +0300

    второй тест, последний этап

--- src/screens/SecondTestScreen.js
+++ src/screens/SecondTestScreen.js
@@ -5,8 +5,8 @@ export default class SecondTestScreen extends Component {
     render() {
         return (
             <View style={{flex: 10, justifyContent: 'flex-start', alignItems: 'flex-start',}}>
-                <View style={{backgroundColor: 'orange', position: 'absolute', height: 900, top: 0, bottom: 60, left: 0, right: 0, margin: 15 }} />
-                <View style={{height: 50, backgroundColor: 'purple', position: 'absolute', bottom: 0, left: 0, right: 0, margin: 15 }} />
+                <View style={{backgroundColor: 'orange', flex: 10, alignSelf: 'stretch', margin: 8}} />
+                <View style={{height: 50, backgroundColor: 'purple', alignSelf: 'stretch', margin: 18 }} />
             </View>
         );
     }
