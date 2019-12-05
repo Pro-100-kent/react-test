9722dc6b77ce8b2e4a687ececa1dfbf5ce546158
Pro-100-kent <pro100kent@gmail.com>
2019-12-05 11:56:08 +0300

    добавили одну кнопку

--- src/screens/InstaTestScreen.js
+++ src/screens/InstaTestScreen.js
@@ -2,30 +2,45 @@ import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 
 export default class InstaTestScreen extends Component {
-    render() {
-        return (
-            <View style={styles.mainContainer}>
-                <View style={styles.actionsContanier} />
-                <View style={styles.listContainer} />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <View style={styles.actionsContainer}>
+          <View style={styles.photoButtonContainer} />
+          <View style={styles.titleContainer} />
+          <View style={styles.bestVideosButtonContainer} />
+          <View style={styles.sendMessageButtonContainer} />
+        </View>
+        <View style={styles.listContainer} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-    },
-    listContainer: {
-        backgroundColor: 'orange',
-        flex: 1,
-        alignSelf: 'stretch',
-    },
-    actionsContanier: {
-        height: 75,
-        alignSelf: 'stretch',
-        
-    },
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  listContainer: {
+    backgroundColor: 'orange',
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  actionsContainer: {
+    height: 75,
+    alignSelf: 'stretch',
+    borderBottomWidth: 3,
+    borderBottomColor: 'gray',
+  },
+  photoButtonContainer: {
+    backgroundColor: 'black',
+    height: 45,
+    width: 45,
+    margin: 15,
+  },
+  titleContainer: {},
+  bestVideosButtonContainer: {},
+  sendMessageButtonContainer: {},
 });
