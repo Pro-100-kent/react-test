36301289e72870cd857202edc6815a7393adfebd
Pro-100-kent <pro100kent@gmail.com>
2019-12-05 10:29:12 +0300

    первоначальный экран инсты

---
+++ src/screens/InstaTestScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class InstaTestScreen extends Component {
+    render() {
+        return (
+            <View style={{flex: 10, justifyContent: 'flex-start', alignItems: 'flex-start',}}>
+                <View style={{backgroundColor: 'orange', flex: 10, alignSelf: 'stretch', margin: 8}} />
+                <View style={{height: 50, backgroundColor: 'purple', alignSelf: 'stretch', margin: 18 }} />
+            </View>
+        );
+    }
+}
